a83f11a5fadc21be3f7a3a83fe9db1b132088d7f
brahiiim <56367267+brahiiim@users.noreply.github.com>
2021-12-18 21:42:03 +0100

    Provider Controller fixes.

--- src/main/java/com/brahim/stockmanagement/controller/ProviderController.java
+++ src/main/java/com/brahim/stockmanagement/controller/ProviderController.java
@@ -4,9 +4,11 @@ import com.brahim.stockmanagement.controller.api.ProviderApi;
 import com.brahim.stockmanagement.dto.ProviderDto;
 import com.brahim.stockmanagement.services.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class ProviderController implements ProviderApi {
 
     private ProviderService providerService;
--- src/main/java/com/brahim/stockmanagement/controller/api/ProviderApi.java
+++ src/main/java/com/brahim/stockmanagement/controller/api/ProviderApi.java
@@ -1,6 +1,7 @@
 package com.brahim.stockmanagement.controller.api;
 
 import com.brahim.stockmanagement.dto.ProviderDto;
+import io.swagger.annotations.Api;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import static com.brahim.stockmanagement.utils.Constants.APP_ROOT;
 
 @RequestMapping("/providers")
+@Api(APP_ROOT + "/providers")
 public interface ProviderApi {
     @PostMapping(value = APP_ROOT + "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ProviderDto save(@RequestBody ProviderDto dto);
