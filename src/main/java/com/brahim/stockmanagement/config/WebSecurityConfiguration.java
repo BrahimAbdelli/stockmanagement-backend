4cb64cefef574e673b6bca02b31e7b3e1e77c42b
brahiiim <56367267+brahiiim@users.noreply.github.com>
2022-01-21 15:26:28 +0100

    Update WebSecurityConfiguration.java

--- src/main/java/com/brahim/stockmanagement/config/WebSecurityConfiguration.java
+++ src/main/java/com/brahim/stockmanagement/config/WebSecurityConfiguration.java
@@ -2,9 +2,10 @@ package com.brahim.stockmanagement.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
